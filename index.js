7b9f68e6c7dc82c757891e21ab567404c9626b81
Chawla-Suhani <suhani.chawla.tech@gmail.com>
2025-03-26 04:03:13 +0530

    created server

--- index.js
+++ index.js
@@ -1,61 +1,48 @@
-import {input} from "@inquirer/prompts";//import funciton input from library @inquirer/prompts
+import express from "express";
 import  qr from "qr-image"
 import fs from "fs";
-import inquirer from 'inquirer';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
-// console.log(typeof inquirer);
-inquirer
-  .prompt([{
-    /* Pass your questions in here */
-    message:"Enter the url",
-    name:"URL"
-    },{
-         message:"Enter your name",
-    name:"name"
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+
+app.listen(port,(req,res)=>{
+  console.log(`Server is running at port ${port}`);
+})
+
+app.get("/",(req,res)=>{
+  res.sendFile(__dirname + "public/index.html");
+})
+
+  app.get("/generate" , (req,res) =>{
+    const q = req.query.q;
+
+    if(!q ){
+      return res.status(400).send("Bad Request : Missing Parameters");
     }
-  ])
-  .then((answer) => {
-    //console.log(answer);
-    let qr_png = qr.image(answer.URL , {type : 'png'});
-    qr_png.pipe(fs.createWriteStream('QR_GENERATED.png'));
 
-    fs.writeFile('url_qr_code.txt',`URL: ${answer.URL}\nNAME : ${answer.name}`,(err)=>{
-      if (err) throw err;
-      console.log("URL of QR_CODE is saved successfully");
-      
-    })
-  });
+    res.setHeader('Content-disposition', 'attachment; filename="QR_CODE.png"'); // Download prompt
+    res.setHeader('Content-Type', 'image/png');
+    //Generate QR Code
+    let qr_png = qr.image(q,{type : 'png' });
 
-  
-//console.log(ans);
-/*************inquirer.prompt([]) returns a promise*********************/
-// a Promise is used to handle tasks that take time, like fetching data from a server. It doesn’t give the result immediately but ensures that when the task is done, you get either:
-// ✔ Success (resolved) → You get the data.
-// ❌ Failure (rejected) → You get an error. 
-
-// A Promise in JavaScript is an object that represents the future result of an asynchronous operation.
-// It acts as a placeholder for a value that isn’t available immediately but will be resolved later.
-
-
-  
-// when the Promise is resolved (successfully completed) then .then() method is called
-
-
-
-
-
-
-
-
-/********ANOTHER METHOD TO GENERATE QR CODE*************/
-
-// const answer = await input({message:"Enter your url for which you want to generate QR Code"});
-
-// fs.writeFile("save_url.txt",answer,(err)=>{
-//     if(err) throw err;
-//     console.log("URL saved successfully");
     
-// });
+    //Save URL and name to a text file
+    
+    //Send QR code to frontend
 
-// var urlQrCode = image(answer,{type:'png'});
-// urlQrCode.pipe(fs.createWriteStream('NewQRCode.png'));
+    res.type('png');
+    qr_png.pipe(res);
+  })
+
+    //Save QR Code , url and name into the file
+    //qr_png.pipe(fs.createWriteStream("QR_GENERATOR.png"));
+    // fs.writeFile('url_qr_code.txt',`URL: ${answer.URL}\nNAME : ${answer.name}`,(err)=>{
+    //   if (err) throw err;
+    //   console.log("URL of QR_CODE is saved successfully");
+    // })
